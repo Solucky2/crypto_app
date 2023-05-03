1f001065d53b3a86fda7a9e58188cc3aa0990aed
Solucky2 <oneksiakprzemek@gmail.com>
2023-05-03 15:25:06 +0200

    dodanie solvera, kodowania i odkodowania dla macierzy, zmiana w dics

--- krypt_app/code_class.py
+++ krypt_app/code_class.py
@@ -1,114 +1,161 @@
 from dics import set, set2
 from MessageClass import EncodeMessages, SolverMessages
 from solver import solve_digram, solve_afinic_code
+import numpy as np
 
 
 class CodeTypes:
 
-
-    def encode_fence_code(self, message:str, height:int): #działa
+    def encode_fence_code(self, message: str, height: int):
         sheet = [["" for i in range(len(message))] for j in range(height)]
         direction = -1
         row = 0
         col = 0
-        for i in message:
-            sheet[row][col] = i
-            if row == 0 or row == height - 1:
+        for letter in message:
+            sheet[row][col] = letter
+            if row == 0 or row == height-1:
                 direction = -direction
             row += direction
             col += 1
-        [print(i) for i in sheet]
+        [print(layer) for layer in sheet]
         out = ''
-        for i in range(height):
-            for j in range(len(message)):
-                out += sheet[i][j]
+        for layer in range(height):
+            for letter in range(len(message)):
+                out += sheet[layer][letter]
         print(out)
 
-
-
-    def decode_cesar_code(self, message:str, b:int): #dziala
+    def decode_cesar_code(self, message: str, b: int):
         out = []
-        for i in message:
-            ind = set.get(i)
-            val = (ind - b) % 26
+        for letter in message:
+            ind = set.get(letter)
+            val = (ind - b) % len(set)
             out.append(val)
-        mess = [set2[i] for i in out]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
+        mess = [set2[number] for number in out]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
 
-
-    def encode_cezar_code(self, message:str,b:int): #dziala
+    def encode_cezar_code(self, message: str, b: int):
         out = []
-        for i in message:
-            ind = set.get(i)
-            val = (ind + b) % 26
+        for letter in message:
+            ind = set.get(letter)
+            val = (ind + b) % len(set)
             out.append(val)
-        mess = [set2[i] for i in out]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
+        mess = [set2[number] for number in out]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
 
-
-    def encode_modulo(self, message:str, b:int): #działa
+    def encode_modulo(self, message: str, b: int):
         outp = []
-        for i in message:
-            a = set.get(i)
-            val = (a + b)%26
+        for letter in message:
+            a = set.get(letter)
+            val = (a+b) % len(set)
             outp.append(val)
+        mess = [set2[number] for number in outp]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
 
-        mess = [set2[i] for i in outp]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
-
-    def decode_modulo(self, message:str, b:int): #działa
+    def decode_modulo(self, message: str, b: int):
         outp = []
-        for i in message:
-            a = set.get(i)
-            val = (a - b)%26
+        for letter in message:
+            a = set.get(letter)
+            val = (a-b)%26
             outp.append(val)
-
-        mess = [set2[i] for i in outp]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
+        mess = [set2[number] for number in outp]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
 
 
-    def encode_decode_module_keys(self, message:str, key1:int, key2:int): #dziala
+    def encode_decode_module_keys(self, message: str, key1: int, key2: int):
         out = []
-        for i in message:
-            a = set.get(i)
-            val = (a*key1+key2)%26
+        for letter in message:
+            a = set.get(letter)
+            val = (a*key1+key2) % len(set)
             out.append(val)
-        mess = [set2[i] for i in out]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
+        mess = [set2[number] for number in out]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
 
-
-    def encode_decode_modulo_digram(self, message:str,a:int, b:int): #działa
+    def encode_decode_modulo_digram(self, message: str, a: int, b: int):
         if len(message) % 2 != 0:
             message += "Q"
         out3 = []
-        sen2 = [message[i:i + 2] for i in range(0, len(message), 2)]
-        out = (26 * set.get(i[0]) + set.get(i[1]) for i in sen2)
-        out2 = ((a * i + b) % 676 for i in out)
+        sen2 = [message[i:i+2] for i in range(0, len(message), 2)]
+        out = (26*set.get(i[0])+set.get(i[1]) for i in sen2)
+        out2 = ((a*i + b) % 676 for i in out)
         for i in out2:
-            val3 = i // 26
-            val4 = i % 26
+            val3 = i//len(set)
+            val4 = i % len(set)
             out3.append(val3)
             out3.append(val4)
-        mess = [set2[i] for i in out3]
-        encoded = ''
-        for _ in mess:
-            encoded += _
-        print(encoded)
+        mess = [set2[number] for number in out3]
+        encoded = ''.join(mess)
+        print("Hasło: " + encoded)
+
+    def decode_matrix_code(self, message: str, a: int, b: int, c: int, d: int):
+        if len(message) % 2 != 0:
+            message += "Q"
+        sen2 = [message[i:i+2] for i in range(0, len(message), 2)]
+        array_keys = np.array([[a, b], [c, d]])
+        out = []
+        for par_of_letters in sen2:
+            for letter in par_of_letters:
+                letter_value_1 = set.get(letter)
+                out.append(letter_value_1)
+        out_sorted_for_two = [out[i:i+2] for i in range(0, len(out), 2)]
+        encoded_values = []
+        for i in out_sorted_for_two:
+            summed_i = np.sum(i*array_keys, axis=1)
+            encoded_values.append(summed_i)
+        encoded_message = []
+        for i in encoded_values:
+            for j in i:
+                val = set2.get(j % len(set))
+                encoded_message.append(val)
+        encoded = ''.join(encoded_message)
+        print("Hasło: " + encoded)
+
+    def encode_matrix_code(self, message: str, a: int, b: int, c: int, d: int):
+        d_factor = a*d-c*b
+        if d_factor < 0:
+            d_factor += len(set)
+        d_factor = pow(d_factor, -1, len(set))
+        sen2 = [message[i:i+2] for i in range(0, len(message), 2)]
+        array_keys = np.array([[d*d_factor, b*-d_factor], [c*-d_factor, a*d_factor]])
+        factored_num = []
+        for row in array_keys:
+            for num in row:
+                if num < 0:
+                    while num < 0:
+                        num += len(set)
+                    factored_num.append(num)
+                elif num > len(set):
+                    while num > len(set):
+                        num -= len(set)
+                    factored_num.append(num)
+                else:
+                    factored_num.append(num)
+        factored_num = np.array(factored_num)
+        factored_num = factored_num.reshape(2, -1)
+        print("Macierz odwrtona: ")
+        print(factored_num)
+        out = []
+        for par_of_letters in sen2:
+            for letter in par_of_letters:
+                letter_value_1 = set.get(letter)
+                out.append(letter_value_1)
+        out_sorted_for_two = [out[i:i + 2] for i in range(0, len(out), 2)]
+        decoded_values = []
+        for i in out_sorted_for_two:
+            summed_i = np.sum(i*array_keys, axis=1)
+            decoded_values.append(summed_i)
+        encoded_message = []
+        for i in decoded_values:
+            for j in i:
+                val = set2.get(j % len(set))
+                encoded_message.append(val)
+        encoded = ''.join(encoded_message)
+        print("Hasło: " + encoded)
+
 
 
 class CodeActions:
@@ -120,6 +167,7 @@ class CodeActions:
             CodeTypes().encode_cezar_code(message, int(b))
         except ValueError:
             print(EncodeMessages().messege_invalid_type)
+
     def cesar_code_actions_decode(self):
         try:
             message = input(EncodeMessages().input_password).upper().replace(" ", "")
@@ -127,6 +175,7 @@ class CodeActions:
             CodeTypes().decode_cesar_code(message, int(b))
         except ValueError:
             print(EncodeMessages().messege_invalid_type)
+
     def fence_code_actions_encode(self):
         try:
             message = input(EncodeMessages().input_password).upper().replace(" ", "")
@@ -134,6 +183,7 @@ class CodeActions:
             CodeTypes().encode_fence_code(message, int(height))
         except ValueError:
             print(EncodeMessages().messege_invalid_type)
+
     def modulo_code_actions_encode(self):
         try:
             message = input(EncodeMessages().input_password).upper().replace(" ", "")
@@ -141,6 +191,7 @@ class CodeActions:
             CodeTypes().encode_modulo(message, int(b))
         except ValueError:
             print(EncodeMessages().messege_invalid_type)
+
     def modulo_code_actions_decode(self):
         try:
             message = input(EncodeMessages().input_password).upper().replace(" ", "")
@@ -176,7 +227,7 @@ class SolverActions:
             out1 = input(SolverMessages().out1_input).replace(" ", "")
             par2 = input(SolverMessages().y_input).replace(" ", "")
             out2 = input(SolverMessages().out2_input).replace(" ", "")
-            solve_afinic_code(par1=par1,par2=par2,out1=out1,out2=out2)
+            solve_afinic_code(par1=par1, par2=par2, out1=out1, out2=out2)
         except ValueError:
             print(SolverMessages().message_invalid_type)
 
@@ -186,6 +237,6 @@ class SolverActions:
             out1 = input(SolverMessages().out1_input).replace(" ", "")
             par2 = input(SolverMessages().y_input).replace(" ", "")
             out2 = input(SolverMessages().out2_input).replace(" ", "")
-            solve_digram(par1=par1,par2=par2,out1=out1,out2=out2)
+            solve_digram(par1=par1, par2=par2, out1=out1, out2=out2)
         except ValueError:
             print(SolverMessages().message_invalid_type)
--- krypt_app/solver.py
+++ krypt_app/solver.py
@@ -1,30 +1,88 @@
 from dics import *
+import numpy as np
 
-def solve_afinic_code(par1:str, par2:str, out1:str, out2:str):
+
+def solve_afinic_code(par1: str, par2: str, out1: str, out2: str):
     x = set.get(par1)
     y = set.get(par2)
     out1 = set.get(out1)
     out2 = set.get(out2)
     for a in range(26):
         for b in range(26):
-            res1 = (a*x+b)%26
-            res2 = (a*y+b)%26
+            res1 = (a*x+b) % 26
+            res2 = (a*y+b) % 26
             if res1 == out1 and res2 == out2:
                 print({'a': a, 'b': b})
 
 
-def solve_digram(par1:str,par2:str, out1:str,out2:str):
+def solve_digram(par1: str, par2: str, out1: str, out2: str):
     par_1_num = [set.get(i) for i in par1]
     par_2_num = [set.get(i) for i in par2]
     x = par_1_num[0]*26+par_1_num[1]
     y = par_2_num[0]*26+par_2_num[1]
-    out1_num =  [set.get(i) for i in out1]
+    out1_num = [set.get(i) for i in out1]
     out2_num = [set.get(i) for i in out2]
     out1 = out1_num[0]*26+out1_num[1]
     out2 = out2_num[0]*26+out2_num[1]
     for a in range(676):
         for b in range(676):
-            res1 = (a*x+b)%676
-            res2 = (a*y+b)%676
+            res1 = (a*x+b) % 676
+            res2 = (a*y+b) % 676
             if res1 == out1 and res2 == out2:
                 print({'a': a, 'b': b})
+
+
+def solve_matrix(par1: str, par2: str, out1: str, out2: str):
+    par_1_num = [set.get(i) for i in par1]
+    par_2_num = [set.get(i) for i in par2]
+    out1_num = [set.get(i) for i in out1]
+    out2_num = [set.get(i) for i in out2]
+    array_keys_text_public = np.transpose(np.array([par_1_num, par_2_num]))
+    array_keys_text_decoded = np.transpose(np.array([out1_num, out2_num]))
+    det_keys_decoded = round(np.linalg.det(array_keys_text_decoded))
+    det_keys_decoded_modulo = det_keys_decoded % len(set)
+    modulo_reverse = pow(det_keys_decoded_modulo, -1, len(set))
+    array_keys_text_decoded_list = []
+    for row in array_keys_text_decoded:
+        for number in row:
+            array_keys_text_decoded_list.append(number)
+    array_keys_text_decoded_reverse = np.array([
+        [array_keys_text_decoded_list[3], array_keys_text_decoded_list[1]*-1],
+        [array_keys_text_decoded_list[2]*-1, array_keys_text_decoded_list[0]]])
+    array_keys_text_decoded_reverse = array_keys_text_decoded_reverse*modulo_reverse
+    c_final = []
+    for row in array_keys_text_decoded_reverse:
+        for num in row:
+            if num < 0:
+                while num < 0:
+                    num += len(set)
+                c_final.append(num)
+            elif num > len(set):
+                while num > len(set):
+                    num -= len(set)
+                c_final.append(num)
+            else:
+                c_final.append(num)
+
+    c_final = np.array(c_final)
+    c_final = c_final.reshape(2, -1)
+    arr_splited1, arr_splited2 = np.split(c_final, [1], axis=1)
+    arr_splited1 = np.array([[arr_splited1[0, 0]*array_keys_text_public[0, 0] +
+                              arr_splited1[1, 0]*array_keys_text_public[0, 1]],
+                             [arr_splited1[0, 0]*array_keys_text_public[1, 0] +
+                              arr_splited1[1, 0]*array_keys_text_public[1, 1]]])
+    arr_splited2 = np.array([[arr_splited2[0, 0]*array_keys_text_public[0, 0] +
+                              arr_splited2[1, 0]*array_keys_text_public[0, 1]],
+                             [arr_splited2[0, 0]*array_keys_text_public[1, 0] +
+                              arr_splited2[1, 0]*array_keys_text_public[1, 1]]])
+    final_array = np.concatenate([arr_splited1, arr_splited2], axis=1)
+    final_array_out = []
+    for row in final_array:
+        for number in row:
+            final_array_out.append(number % len(set))
+    final_array_out = np.array(final_array_out)
+    final_array_out = final_array_out.reshape(2, -1)
+    print("Macierz rozszyfrowująca: ")
+    print(final_array_out)
+
+
